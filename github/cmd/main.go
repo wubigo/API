1608655d9e20ec09ce340ae55ce6fca1cf73bcff
bigo <wayatwork@gmail.com>
2019-03-25 15:24:00 +0800

    commit a new file

--- github/cmd/main.go
+++ github/cmd/main.go
@@ -9,10 +9,12 @@
 package main
 
 import (
-"context"
-"fmt"
+	"context"
+	"fmt"
+	"golang.org/x/oauth2"
+	"reflect"
 
-"github.com/google/go-github/v24/github"
+	"github.com/google/go-github/github"
 )
 
 // Fetch all the public organizations' membership of a user.
@@ -29,6 +31,44 @@ func FetchRepos(username string) ([]*github.Repository, error) {
 	client := github.NewClient(nil)
 	repos, _, err := client.Repositories.List(context.Background(), username, nil)
 	return repos, err
+}
+
+//commit a new file with `content`
+func CreateFile(repo, path string) {
+	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: "at"})
+	ctx := context.Background()
+	tc := oauth2.NewClient(ctx, ts)
+	client := github.NewClient(tc)
+	if client == nil {
+
+	}
+	message := "m"
+	content := []byte("file content")
+
+	repositoryContentsOptions := &github.RepositoryContentFileOptions{
+		Message:   &message,
+		Content:   content,
+		Committer: &github.CommitAuthor{Name: github.String("n"), Email: github.String("e")},
+	}
+
+	createResponse, _, err := client.Repositories.CreateFile(ctx, "wubigo", repo, path, repositoryContentsOptions)
+	if err != nil {
+		fmt.Printf("Repositories.CreateFile returned error: %v\n", err)
+	}
+	want := &github.RepositoryContentResponse{
+		Content: &github.RepositoryContent{Name: github.String("p")},
+		Commit: github.Commit{
+			Message: github.String("m"),
+			SHA:     github.String("f5f369044773ff9c6383c087466d12adb6fa0828"),
+		},
+	}
+	if !reflect.DeepEqual(createResponse, want) {
+		fmt.Printf("Repositories.CreateFile returned %+v, want %+v\n", createResponse, want)
+	}
+
+
+
+
 }
 
 func main() {
@@ -62,9 +102,10 @@ func main() {
 
 	} else {
 		for i, repo := range repos {
-			fmt.Printf("%v. %v\n", i+1, repo.GetName())
+			fmt.Printf("%v. name=%v url=%v\n", i+1, repo.GetName(), repo.GetCloneURL())
 		}
 
 	}
-}
 
+	CreateFile("API", "kabla.yaml")
+}
