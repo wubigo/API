5135bf850731d7c070f32d24f0049f2c3a0c8115
bigo <wayatwork@gmail.com>
2019-06-26 11:03:40 +0800

    flink scala

---
+++ java/flink-scala/src/test/java/bigo/flink/scala/F1ApplicationTests.java
@@ -0,0 +1,16 @@
+package bigo.flink.scala;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class F1ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
